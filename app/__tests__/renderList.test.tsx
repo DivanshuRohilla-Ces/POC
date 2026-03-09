7a19e81142c4822ab6a541e88496ee3390a14d97
Divanshu <divanshu.rohilla@cesltd.com>
2026-03-09 12:20:15 +0530

    Refactored the code according to the given feedback

--- app/__tests__/renderList.test.tsx
+++ app/__tests__/renderList.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import RenderUsers from "../renderList";
+import RenderUsers from "../../components/renderList";
 
 const mockUser = {
   firstName: "Emily",
